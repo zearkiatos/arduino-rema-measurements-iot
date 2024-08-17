44f9f0a4679a419b3ac61fea23d1e8ba55009ade
zearkiatos <caprilespe@outlook.com>
2024-08-17 15:21:01 -0400

    Adding brightness intensity challenge feature

---
+++ Brightness_Intensity/configexample.h
@@ -0,0 +1,12 @@
+// config.h
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define SSID ""
+#define PASSWORD ""
+#define MQTT_PASSWORD ""
+#define HOSTNAME ""
+#define MQTT_USER ""
+#define LOCATION ""
+
+#endif
